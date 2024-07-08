d13d033c304a3a09ce1a377644cdb260cef3a08f
souls <115924743+souls2108@users.noreply.github.com>
2024-07-08 20:05:45 +0530

    express app setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env',
 })
 
-connectDB();
+const port = process.env.PORT || 8000;
+
+connectDB()
+.then( () => {
+    try {
+        app.on("error", (error) => {
+            console.error("-> EXPRESS ERROR: ", error);
+            throw error;
+        });
+        app.listen(port, () => {
+            console.log(`-> App listening at port : ${port}`);
+        });
+        
+    } catch (error) {
+        console.error("EXPRESS ERROR: ", error);
+        throw err;
+    }
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed.");
+});
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -8,14 +8,3 @@ const asyncHandler = (requestHandler) => {
 
 
 export {asyncHandler}
-
-// const asyncHandler = (fn) => async (req, res, next) => {
-//     try {
-//         await finally(req, res, next);
-//     } catch (error) {
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: err.message
-//         })
-//     }
-// }
