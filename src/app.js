0c63aa847a45aa83e03eeb02f15059c48fb4686e
souls <115924743+souls2108@users.noreply.github.com>
2024-12-05 01:10:04 +0530

    Implement dynamic CORS origin handling

--- src/app.js
+++ src/app.js
@@ -4,8 +4,16 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+const allowedOrigins = process.env.CORS_ORIGIN.split(',');
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);          
+      } else {          
+          callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true
 }))
 
